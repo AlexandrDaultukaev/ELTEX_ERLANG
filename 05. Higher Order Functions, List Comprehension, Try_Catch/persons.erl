c6d80fe2c09b92a9f0c0998eb29aeae8a2549f59
Alexandr Daultukaev <alexandrdaultukaev@gmail.com>
2023-03-18 20:18:45 +0700

    Add 5th lab

---
+++ 05. Higher Order Functions, List Comprehension, Try_Catch/persons.erl
@@ -0,0 +1,24 @@
+-module(persons).
+
+% rr("person.hrl").
+% Persons = [#person{id=1, name="Bob", age=23, gender=male}, #person{id=2, name="Kate", age=20, gender=female}, #person{id=2, name="Jack", age=34, gender=male}, #person{id=4, name="Nata", age=54, gender=female}].
+-include("person.hrl").
+
+-export([filter/2, all/2, any/2, update/2, get_average_age/1]).
+
+% persons:filter((fun(#person{age = Age}) -> Age >= 30 end, Persons)
+filter(Fun, Persons) -> lists:filter(Fun, Persons).
+all(Fun, Persons) -> lists:all(Fun, Persons).
+any(Fun, Persons) -> lists:any(Fun, Persons).
+update(Fun, Persons) -> lists:map(Fun, Persons).
+
+
+get_average_age([]) -> io:format("List can't be empty!\n"), {error, badarg};
+get_average_age(Persons) ->
+    Get_persons_number_and_age_sum = fun(#person{age = Age}, {Count, AgeSum}) -> {Count+1, AgeSum+Age} end,
+    {_PersonsCount, _AgeSum} = lists:foldl(Get_persons_number_and_age_sum, {0, 0}, Persons),
+    {ok, _AgeSum/_PersonsCount}.
+
+% UpdateJackAge = fun(
+%     #person{name = "Jack", age = Age} = Person) -> Person#person{age=Age + 1};
+%     (Person) -> Person end.
